c15cc53f2a4fe9053001a2ae7465ddeab837940c
Dorian <dorianmeade1@gmail.com>
2019-11-05 18:41:56 -0500

    done without style

--- src/components/ViewBuilding.js
+++ src/components/ViewBuilding.js
@@ -26,13 +26,8 @@ class ViewBuilding extends React.Component {
 					<p>Building Info</p>
 						{info.name ? <ul>Name: {info.name}</ul> : ' '}
 						{info.code ? <ul>Code: {info.code}</ul> : ' '}
-						{
-							info.coordinates ? 
-							(info.coordinates.longitude ? <ul>Longitude: {info.coordinates.longitude}</ul> : ' ') &&				
-							(info.coordinates.latitude ? <ul>Latitude: {info.coordinates.latitude}</ul> : ' ')
-							: ' '	
-						}
-										
+						{info.coordinates ? <ul>Latitude: {info.coordinates.latitude}</ul> : ''}
+						{info.coordinates ? <ul>Longitude: {info.coordinates.longitude}</ul> : ''}
 						{info.address ? <ul>Address: {info.address}</ul> : ' '}
 				</div>
 			);
